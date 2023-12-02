72e9c901e15eeb5172debe0e98a91c35ab964145
Hsu-Hsiang Tsao <hsuhsiang.tsao@gmail.com>
2023-12-01 18:08:20 -0800

    Update Algorithm Relating to Hensel's Lemma.py

--- Algorithm Relating to Hensel's Lemma/Algorithm Relating to Hensel's Lemma.py
+++ Algorithm Relating to Hensel's Lemma/Algorithm Relating to Hensel's Lemma.py
@@ -1,5 +1,10 @@
+'''
+Note that the algorithm's runtime may be improved significantly by implementing the Euclidean Algorithm or the Magic Box Algorithm in the determination of epsilon. 
+However, the algorithm does generate solutions sufficiently fast for adequately small n.
+'''
+
 def bash(p,a,n):
-    _, epsion = 0,1
+    _, epsilon = 0,1
     while True: 
         if (_**2-a) % p == 0: #Determining b_1
             L=[_]
@@ -8,10 +13,10 @@ def bash(p,a,n):
     for j in range(1,n+1):
         B_j = L[j-1]  #b_{j-1}
         while True:
-            numerator = (2*B_j*epsion) - a + (B_j**2)
+            numerator = (2*B_j*epsilon) - a + (B_j**2)
             if numerator % p**j==0:
-                b_j = B_j + epsion
+                b_j = B_j + epsilon
                 L.append(b_j)
                 break
-            epsion+=1
+            epsilon+=1
     print(f"b_{n} = {L[n]}, where {L[n]}^2 ≡ {a} (mod {p}^{n})")
